673eb64423a3c400f574830f9bb68ac5c54a5863
juan.karsten <juan.karstens@gmail.com>
2014-10-03 14:18:58 +0700

    dependency injection

--- src/test/java/com/apress/isf/java/service/LocalSearchEngineTest.java
+++ src/test/java/com/apress/isf/java/service/LocalSearchEngineTest.java
@@ -3,21 +3,31 @@ package com.apress.isf.java.service;
 import java.util.List;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.apress.isf.java.model.Document;
 import com.apress.isf.java.model.Type;
 
 public class LocalSearchEngineTest {
-	private SearchEngine localSearchEngine = new LocalSearchEngine();
+	private SearchEngine localSearchEngine;
+	private Type type;
+	private ClassPathXmlApplicationContext context;
 	
+	@Before
+	public void setup() {
+		context = 
+				new ClassPathXmlApplicationContext("META-INF/spring/beancontext.xml");
+		localSearchEngine = context.getBean(SearchEngine.class);
+		type = context.getBean(Type.class);
+	}
 	
 	@Test
 	public void findByType(){
-		Type type = new Type();
-		type.setName("pdf");
-		type.setExtension("pdf");
-		type.setDesc("file pdf");
+		
+		
+		Type type = this.type;
 		
 		List<Document> documents = localSearchEngine.findByType(type);
 		Assert.assertEquals(1, documents.size());
@@ -34,5 +44,6 @@ public class LocalSearchEngineTest {
 		
 		List<Document> documents = localSearchEngine.findByType(type);
 		Assert.assertEquals(1, documents.size());
+		
 	}
 }
