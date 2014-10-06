2d93e8c26a57ec81335c54ca7e06da43f86a4c7a
juan.karsten <juan.karstens@gmail.com>
2014-10-06 09:27:10 +0700

    [chpater 6] add document test

--- src/test/java/com/apress/isf/java/service/MyDocumentsTest.java
+++ src/test/java/com/apress/isf/java/service/MyDocumentsTest.java
@@ -2,9 +2,14 @@ package com.apress.isf.java.service;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.Resource;
 
 public class MyDocumentsTest {
 	private ClassPathXmlApplicationContext context;
@@ -16,8 +21,15 @@ public class MyDocumentsTest {
 	}
 	
 	@Test
-	public void testMenu() {
-		
+	public void testMenu() throws IOException {
+		Resource resource = context.getResource("classpath:META-INF/data/menu.txt");
+		InputStream inputStream = resource.getInputStream();
+		Scanner sc = new Scanner(inputStream);
+		while(sc.hasNext()){
+			System.out.println(sc.nextLine());
+		}
+		sc.close();
+		inputStream.close();
 	}
 
 }
